af17d9407e5194ead63ddd5c2b6efaa4eb97cd68
juan-solorzano <juan.solorzano@globant.com>
2016-02-02 10:40:53 -0600

    agragando usuario

--- chefdocs/metadata.rb
+++ chefdocs/metadata.rb
@@ -1,7 +1,2 @@
 name 'chefdocs'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-description 'Installs/Configures chefdocs'
-long_description 'Installs/Configures chefdocs'
-version '0.1.0'
+
